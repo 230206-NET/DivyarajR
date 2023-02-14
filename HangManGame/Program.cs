545a5bfd5fbbfd9599f2d91a67c0a7131dbed84b
dicode04 <divyarajrana04@gmail.com>
2023-02-13 22:55:46 -0600

    updated changes

--- HangManGame/Program.cs
+++ HangManGame/Program.cs
@@ -1,133 +1,187 @@
 ﻿using System;
-using System.Text.RegularExpressions;
+using System.Text;
 
-namespace HangManGame
+namespace Hangman
 {
-    public class HangManGame
+    public class HangmanGame
     {
-        public static void Main(String[] args)
+        public static void main(string[] args)
         {
-            // List of words that can be used for the game.
-             var words = new[]
+
+            Console.WriteLine("Welcome to Hangman!");
+            HangmanGame newGame = new HangmanGame();
+            newGame.InitGame();
+            newGame.PlayGame();
+        }
+        public String[] words = new String[] {"coffee", "banana", "airport", "cryptography", "computer"};
+        public int lives;
+        public StringBuilder? lettersUsed;
+
+        public String? secretWord;
+        public char[]? theWord;
+
+        public bool keepAlive = true;
+
+        public void InitGame()
+        {
+            lives = 10;
+            lettersUsed = new StringBuilder();
+            Random rand = new Random();
+            int wordIndex = rand.Next(0, words.Length);
+            secretWord = words[wordIndex];         
+            theWord = new char[secretWord.Length];
+            for (int i = 0; i < theWord.Length; i++)
             {
-                "coffee",
-                "banana",
-                "airport",
-                "cryptography",
-                "computer",
-            };
+                theWord[i] = '_';
 
-            // Pick a random word from the array.
-            var chosenWord = words[new Random().Next(0, words.Length - 1)];
-
-            // Regex with valid characters (single character between a and z).
-            var validCharacters = new Regex("^[a-z]$");
-
-            // Number of lives the player has before loosing.
-            var lives = 5;
-
-            // Empty array that will contain all the letters submitted by the player.
-            var letters = new List<string>();
-
-            // As long as there are lives left, the loop continues
-            while (lives != 0)
-            {
-                // Counter of characters left to guess.
-                var charactersLeft = 0;
-            
-                // Loop through all the characters of the word
-                foreach (var character in chosenWord)
-                {
-                    // Make the letter a string (easier for conditions).
-                    var letter = character.ToString();
-                    
-                    // If the letter in the loop is in our array of used letters, we write
-                    // the letter, otherwise we show an underscore (as a letter left to
-                    // guess).
-                    if (letters.Contains(letter))
-                    {
-                        Console.Write(letter);
-                    }
-                    else
-                    {
-                        Console.Write("_");
-                        
-                        // We also increase the count of letters left, used to track whether
-                        // the game is finished or not.
-                        charactersLeft++;
-                    }
-                }
-                Console.WriteLine(string.Empty);
-
-                // If there are no characters left, the game is over and we can break out
-                // of the loop.
-                if (charactersLeft == 0)
-                {
-                    break;
-                }
-
-                Console.Write("Type in a letter: ");
-                
-                // Read the input character and transform it to lowercase to make it easier
-                // to compare to already used letters (we could fall into the scenario of a
-                // player inputting a capitalized letter that has already been submitted). 
-                var key = Console.ReadKey().Key.ToString().ToLower();
-                Console.WriteLine(string.Empty);
-
-                // Using the declared Regex above, we check if the the submitted character
-                // is valid or not.
-                if (!validCharacters.IsMatch(key))
-                {
-                    // If the character is invalid, we loop back to the beginning using the
-                    // "continue" statement and let the user know of the error.
-                    Console.WriteLine($"The letter {key} is invalid. Try again.");
-                    continue;
-                }
-
-                // Is the letter already in our array of used letters? 
-                if (letters.Contains(key))
-                {
-                    // Only show a message to the user that the letter has already been
-                    // used. We don't want to do anything else (for example storing it)
-                    // with that.
-                    Console.WriteLine("You already entered this letter!");
-                    continue;
-                }
-
-                // If the letter has not already been used, we add it to our array of
-                // letters.
-                letters.Add(key);
-
-                // If the chosen word doesn't contain the given letter, we reduce the
-                // number of lives left by one.
-                if (!chosenWord.Contains(key))
-                {
-                    lives--;
-
-                    // If all the lives ran out, there's no point saying how many lives
-                    // are left as the loose message will be shown.
-                    if (lives > 0)
-                    {
-                        // Here, a ternary is used in the string to either how a pluralized
-                        // version of "try" or the singular one, depending on how many
-                        // lives are left.
-                        Console.WriteLine($"The letter {key} is not in the word. You have {lives} {(lives == 1 ? "try" : "tries")} left.");
-                    }
-                }
-            }
-
-            // If the user has lives left, we display the win message, otherwise we don't
-            if (lives > 0)
-            {
-                // This uses a ternary to pluralize the word lives unless there only one
-                // life left.
-                Console.WriteLine($"You won with {lives} {(lives == 1 ? "life" : "lives")} left!");
-            }
-            else
-            {   
-                Console.WriteLine($"You lost! The word was {chosenWord}.");
-                   
             }
         }
+
+
+        public void PlayGame()
+        {
+            while (keepAlive)
+            {
+                PrintStatus();
+                Console.WriteLine("Press W to guess a word, L to guess a letter or Q to quit:");
+                String choice = Console.ReadLine();
+                String action = choice.ToUpper();
+                switch (action)
+                {
+                    case "W":
+                        Console.WriteLine("Which word do you think is right?");
+                        String guessedWord = Console.ReadLine();
+                        GuessWord(guessedWord.ToLower());
+                        break;
+                    case "L":
+                        Console.WriteLine("Which letter would you like to guess for?");
+                        String guessedLetter = Console.ReadLine();
+                        char letter = guessedLetter[0];
+                        GuessLetter(letter);
+                        break;
+                    case "Q":
+                        Console.WriteLine("Thank you for playing!");
+                        keepAlive = false;
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid option.");
+                        break;
+                }
+                if (lives == 0)
+                {
+                    Console.WriteLine("You have no more guesses! Game over.");
+                    EndOfGame();
+                }
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        public void PrintStatus()
+        {
+            Console.WriteLine("You have " + lives + " guesses left.");
+            Console.WriteLine("you have guessed the following letters: " + lettersUsed);
+            Console.WriteLine(theWord);
+        }
+
+        void GuessWord(String word)
+        {
+            lives--;
+            if (word.Equals(secretWord))
+            {
+                Console.WriteLine("Congratulations! You have guessed the right word.");
+                EndOfGame();
+            }
+            else
+            {
+                Console.WriteLine("Wrong word!");
+            }
+        }
+
+         void GuessLetter(char letter)
+        {
+            String letters = lettersUsed.ToString();
+            if (letters.Contains(letter))
+            {
+                Console.WriteLine("You have already made that guess!");
+            }
+            else
+            {
+                lives--;
+                lettersUsed.Append(letter); 
+                if (IsInWord(letter))
+                {
+                    Console.WriteLine("Yay! The letter " + letter + " is in the word.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, the letter " + letter + " is not in the word.");
+                }
+            }
+            if (StringEqualsCharArray(secretWord, theWord))
+            {
+                Console.Write("Congratualtions! You have guessed all the right characters.\n" +
+                    "The word is " + secretWord + ".\n");
+                EndOfGame();
+            }
+        }
+
+        public bool IsInWord(char letter)
+        {
+            bool letterIsInWord = false;
+
+            for (int i = 0; i < theWord.Length; i++)
+            {
+                if (secretWord[i].Equals(letter))
+                {
+                    theWord[i] = letter;
+                    letterIsInWord = true;
+
+                }
+            }
+
+            return letterIsInWord;
+        }
+
+
+         public bool StringEqualsCharArray(String str, char[] chars)
+        {
+            if (str.Length == chars.Length)
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] != chars[i]) return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        void EndOfGame()
+        {
+            Console.WriteLine("Would you like to play again? Y/N");
+            String answer = Console.ReadLine();
+            String ans = answer.ToUpper();
+            if (ans.Equals("N"))
+            {
+                Console.WriteLine("Goodbye, see you later!");
+                keepAlive = false;
+            }
+            else if (ans.Equals("Y"))
+            {
+
+                Console.WriteLine("Go ahead!");
+                InitGame();
+            }
+            else
+            {
+                Console.WriteLine("Please choose Y or N");
+                EndOfGame();
+            }
+
+        }
+
     }
+
 }
